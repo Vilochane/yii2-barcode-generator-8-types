31652f38e498cd6c3dac14403830fa7594a227e2
Vilochane <vilochane@gmail.com>
2015-04-29 23:29:37 +0530

    added changes

---
+++ BarcodeGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace barcode\barcode;
+
+/**
+ * This is just an example.
+ */
+class BarcodeGenerator extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
