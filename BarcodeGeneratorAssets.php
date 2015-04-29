99acac16209a4a36b17c69967efe3b1a77bb79a7
Vilochane <vilochane@gmail.com>
2015-04-30 01:11:23 +0530

    added changes

---
+++ BarcodeGeneratorAssets.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Assets Class
+ * Wrapper  for BarCode Coder Library (BCC Library)
+ *  BCCL Version 2.0
+ *    
+ *  Porting : jQuery barcode plugin 
+ *  Version : 2.0.3
+ *   
+ *  Date    : 2013-01-06
+ *  Author  : DEMONTE Jean-Baptiste <jbdemonte@gmail.com>
+ *            HOUREZ Jonathan
+ *             
+ *  Web site: http://barcode-coder.com/
+ *  dual licence :  http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html
+ *                  http://www.gnu.org/licenses/gpl.html * 
+ * @author Vilochane <vilochane@gmail.com>
+ * @link GitHub https://github.com/Vilochane
+ * @link yii http://www.yiiframework.com/forum/index.php/user/223499-vilo/
+ */
+
+namespace yii\widgets;
+use yii\web\AssetBundle;
+
+class BarcodeGeneratorAssets extends yii\web\AssetBundle {
+
+    /**
+     * @inherit doc
+     */
+    public function init() {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('js', ['js/jquery.maskMoney']);
+        parent::init();
+    }
+
+}
